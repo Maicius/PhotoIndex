1acaaa3d115a9904b6fcf49bad72c3da7727a287
Maicius <maicius@outlook.com>
2019-12-26 17:47:22 +0800

    init

---
+++ image_json.js
@@ -0,0 +1,7 @@
+var image_json = {
+    "title": "小麦冬",
+    "sub_title": "不会骑车的摄影师不是好的程序猿",
+    "back": ["back/back_1.jpg", "back/back_2.jpg", "back/back_3.jpg", "back/back_4.jpg"],
+    "part": [],
+    "page_title": "2019年度影集",
+};
